445d4a031bc1ce644437e75da03749e20392d907
Mehdi Paak <mehdi.paak@contractors.roche.com>
2023-04-20 15:50:52 -0400

    modified build_ace.sh for multistage build dev and prod

--- build/build_ace.sh
+++ build/build_ace.sh
@@ -1,4 +1,17 @@
+#!/bin/bash
+
 cd ace
-    echo "Build APPIAN ACE"
-    docker build --no-cache . --tag ace/appian:latest
+
+    if [[ "$1" == "dev" ]]
+    then 
+        echo "Build APPIAN ACE DEV"
+        docker build --no-cache . --target $1 --tag ace/appian:dev
+    elif [[ "$1" == "prod" ]]
+    then
+        echo "Build APPIAN ACE PROD"
+        docker build --no-cache . --target $1 --tag ace/appian:latest
+    else
+        echo "bad input $1, specify either dev or prod"
+        exit -1
+    fi
 cd ..
